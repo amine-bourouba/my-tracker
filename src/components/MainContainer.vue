030cd3410b6067250179c61c03f6a6b81da4a603
amine bourouba <aminebourouba9@gmail.com>
2024-02-22 11:32:57 +0100

    FEAT: Add MainContainer and Landing components

---
+++ src/components/MainContainer.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="w-[45rem] h-[65rem] | border-solid border-2 rounded-2xl | relative | grid justify-items-center">
+    <div class="absolute top-0">
+      <task-list />
+    </div>
+    <div class="absolute bottom-0 pb-8">
+      <task-editor />
+    </div>
+  </div>
+</template>
+<script setup>
+import TaskEditor from './TaskEditor.vue'
+import TaskList from './TaskList.vue'
+</script>
---
+++ src/views/Landing.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="bg-gray-900">
+    <!-- Header -->
+    <header class="absolute inset-x-0 top-0 z-50">
+      <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+        <div class="flex lg:flex-1" />
+        <div class="hidden lg:flex lg:gap-x-12">
+          <a v-for="item in navigation" :key="item.name" :href="item.href"
+            class="text-sm font-semibold leading-6 text-white">{{ item.name }}</a>
+        </div>
+        <div class="hidden lg:flex lg:flex-1 lg:justify-end" />
+      </nav>
+    </header>
+
+    <main class="relative isolate">
+      <!-- Background -->
+      <div class="absolute inset-x-0 top-4 -z-10 flex transform-gpu justify-center overflow-hidden blur-3xl"
+        aria-hidden="true">
+        <div class="aspect-[1108/632] w-[69.25rem] flex-none bg-gradient-to-r from-[#80caff] to-[#4f46e5] opacity-25"
+          style="clip-path: polygon(73.6% 51.7%, 91.7% 11.8%, 100% 46.4%, 97.4% 82.2%, 92.5% 84.9%, 75.7% 64%, 55.3% 47.5%, 46.5% 49.4%, 45% 62.9%, 50.3% 87.2%, 21.3% 64.1%, 0.1% 100%, 5.4% 51.1%, 21.4% 63.9%, 58.9% 0.2%, 73.6% 51.7%)" />
+      </div>
+
+      <!-- Main section -->
+      <div class="px-6 pt-14 lg:px-8">
+        <div class="mx-auto pt-24">
+          <h2 class="font-bold tracking-tight text-white text-6xl mb-16">My Tracker</h2>
+          <main-container />
+        </div>
+      </div>
+    </main>
+
+    <!-- Footer -->
+    <footer class="relative mt-32 sm:mt-40" aria-labelledby="footer-heading">
+      <h2 id="footer-heading" class="sr-only">Footer</h2>
+      <div class="mx-auto max-w-7xl px-6 pb-8 lg:px-8">
+        <div class="xl:grid xl:grid-cols-3 xl:gap-8">
+          <div class="space-y-8">
+            <h3 class="text-sm font-semibold leading-6 text-white">Good To Know</h3>
+            <ul role="list" class="mt-6 space-y-4">
+              <li v-for="item in solutions" :key="item.name">
+                <a :href="item.href" class="text-sm leading-6 text-gray-300 hover:text-white">{{ item.name }}</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="mt-16 border-t border-white/10 pt-8 sm:mt-20 lg:mt-24">
+          <p class="text-xs leading-5 text-gray-400">&copy; 2024 Oooops Tech, Inc. All rights reserved.</p>
+        </div>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script setup>
+import MainContainer from '../components/MainContainer.vue'
+
+const navigation = [
+  { name: 'Product', href: '#' },
+  { name: 'Features', href: '#' },
+  { name: 'Marketplace', href: '#' },
+]
+const solutions = [
+  { name: 'Marketing', href: '#' },
+  { name: 'Analytics', href: '#' },
+  { name: 'Commerce', href: '#' },
+]
+</script>
